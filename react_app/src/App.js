082f47ffa6ce891e2715bbf342f5f5950441beb4
moyongkexing <moyongkexing@gmail.com>
2021-02-07 22:41:24 +0900

    ステートで表示を切り替える

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  msgStyle = {
+  msgStyle1 = {
     fontSize: "24px",
     color: "#900",
     margin: "20px 0px",
     padding: "5px",
     borderBottom: "2px solid #900"
+  }
+  msgStyle1 = {
+    fontSize: "24px",
+    color: "white",
+    margin: "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solid #900",
+    backgroundColor: "#900"
   }
   btnStyle = {
     fontSize: "20pt",
@@ -19,21 +27,27 @@ class App extends Component {
     this.state = {
       counter: 0,
       msg: "count start!",
+      flg: true,
     };
     this.doAction = this.doAction.bind(this);
   }
 
-  doAction(e) {
+  doAction() {
     this.setState((state) => ({
       counter: state.counter + 1,
-      msg: "counter: " + state.counter
+      msg: state.counter,
+      flg: !state.flg
     }));
   }
 
   render() {
     return <div>
       <h1>React</h1>
-      <p style={this.msgStyle}>{this.state.msg}</p>
+      {this.state.flg ?
+        <p style={this.msgStyle1}>count: {this.state.msg}</p>
+      :
+        <p style={this.msgStyle2}>{this.state.msg}です。</p>
+      }
       <button style={this.btnStyle} onClick={this.doAction}>Click</button>
     </div>;
   }
