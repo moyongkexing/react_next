5f1de8c524141c163420de1dbdfb73d43f2b8038
moyongkexing <moyongkexing@gmail.com>
2021-02-08 14:57:45 +0900

    簡易メモを作る

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,36 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
 import './index.css';
 import App from './App';
+import MemoStore from './memo/Store'
 
-let state_value = {
-  counter: 0,
-  message: "counter"
-}
 
-function counter(state = state_value, action) {
-  switch(action.type) {
-    case "INCREMENT" :
-      return {
-        counter: state.counter + 1,
-        message: "INCREMENT"
-      };
-    case "DECREMENT" :
-      return {
-        counter: state.counter - 1,
-        message: "DECREMENT"
-      };
-    default:
-      return state;
-  }
-}
-
-let store = createStore(counter)
+// 表示をレンダリング
 ReactDOM.render(
-  <Provider store={store}>
-    <App />,
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={MemoStore}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
