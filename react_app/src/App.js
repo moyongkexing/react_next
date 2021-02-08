53bb94dfaeaffbdab5a889e0f1f7efe9800d328c
moyongkexing <moyongkexing@gmail.com>
2021-02-08 11:06:26 +0900

    onCheckをイベントを持ったコンポーネント

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -10,89 +10,47 @@ class App extends Component {
     margin: "20px 0px",
     padding: "5px",
   }
-  inputStyle = {
-    fontSize: "12pt",
-    padding: "5px"
-  }
   constructor(props) {
     super(props);
     this.state = {
       message: "type your name:"
     };
-    this.doChange = this.doChange.bind(this)
-    this.doSubmit = this.doSubmit.bind(this)
+    this.doCheck = this.doCheck.bind(this);
   }
-
-  doChange(event) {
-    this.input = event.target.value;
-  }
-
-  doSubmit(event) {
-    this.setState({
-      message: "Hello," + this.input
-    });
-    event.preventDefault();
+  
+  doCheck(event) {
+    alert(event.target.value + "は長すぎます（最大10文字）");
   }
 
   render() {
     return <div>
       <h1>React</h1>
       <h2>{this.state.message}</h2>
-      <form onSubmit={this.doSubmit}>
-        <label>
-          <span style={this.inputStyle}></span>Message:
-          <input type="text" style={this.inputStyle} onChange={this.doChange} />
-        </label>
-        <input type="submit" style={this.inputStyle} value="Click" />
-      </form>
+      <Message maxlength="10" onCheck={this.doCheck}/>
     </div>;
   }
 }
 
-class List extends Component {
-  number = 1;
-
-  title = {
-    fontSize: "20pt",
-    fontWeight: "bold",
-    color: "blue"
-  };
-
-  render() {
-    let data = this.props.data;
-    return (
-      <div>
-        <p style={this.title}>{this.props.title}</p>
-        <ul>
-          {data.map((item) =>
-            <Item number={this.number++} value={item} key={this.number} />
-          )}
-        </ul>
-      </div>
-    );
+class Message extends Component {
+  inputStyle = {
+    fontSize: "12pt",
+    padding: "5px"
   }
-}
 
-class Item extends Component {
-  li = {
-    listStyleType: "square",
-    fontSize: "16pt",
-    color: "#06",
-    margin: "0px",
-    padding: "0px"
+  constructor(props) {
+    super(props);
+    this.doChange = this.doChange.bind(this)
   }
-  num = {
-    fontWeight: "bold",
-    color: "red"
+
+  doChange(e) {
+    if (e.target.value.length > this.props.maxlength) {
+      this.props.onCheck(e);
+      e.target.value.substr(0,this.props.maxlength);
+    }
   }
 
   render() {
-    return (
-      <li style={this.li}>
-        <span style={this.num}>「{this.props.number}」</span>
-        {this.props.value}
-      </li>
-    );
+    return <input type="text" onChange={this.doChange} style={this.inputStyle} />
   }
 }
 export default App;
