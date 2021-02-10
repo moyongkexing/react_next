d504514df44e99ba124097eb4db2fb85e205f7bf
moyongkexing <moyongkexing@gmail.com>
2021-02-10 15:38:51 +0900

    Reactにfirebaseを組み込む

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,76 +1,38 @@
 import react, { Component } from 'react';
-import { connect } from 'react-redux';
 import './App.css';
+import SampleData from './fire/SampleData';
+import firebase from "firebase";
+
+// Firebaseの設定
+var config = {
+  apiKey: "AIzaSyBzVyx_37_1xJj1UCiQH4xMOZGEYdPFzhM",
+  authDomain: "sue-react.firebaseapp.com",
+  databaseURL: "https://sue-react-default-rtdb.firebaseio.com",
+  projectId: "sue-react",
+  storageBucket: "sue-react.appspot.com",
+  messagingSenderId: "240951514920",
+  appId: "1:240951514920:web:5ec1312a42bb4a76c3a21d",
+  measurementId: "G-33XWHTJXSL"  
+};
+
+// Firebaseの初期化
+firebase.initializeApp(config);
+
 
 // Appコンポーネント
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+
 
   render() {
     return (
       <div>
-        <h1>Redux</h1>
-        <Message />
-        <Button />
+        <h1>Fire</h1>
+        <h2>Sample data.</h2>
+        <SampleData />
       </div>
     );
   }
 }
 
-// ストアのコネクト
-App = connect()(App);
-
-// メッセージ表示のコンポーネント
-class Message extends Component {
-  style = {
-    fontSize: "20pt",
-    padding: "20px 5px"
-  }
-
-  render() {
-    return (
-      <p style={this.style}>
-        {this.props.message}: {this.props.counter}
-      </p>
-    );
-  }
-}
-
-// ストアのコネクト
-Message = connect((state) => state)(Message);
-
-class Button extends Component {
-  style = {
-    fontSize: "16pt",
-    padding: "5px 10px"
-  }
-
-  constructor(props) {
-    super(props);
-    this.doAction = this.doAction.bind(this);
-  }
-
-  doAction(e){
-    if (e.shiftKey){
-      this.props.dispatch({ type:'DECREMENT' });
-    } else if (e.metaKey){
-      this.props.dispatch({ type:'RESET' });
-    } else {
-      this.props.dispatch({ type:'INCREMENT' });
-    }
-  }
-
-  render() {
-    return (
-      <button style={this.style} onClick={this.doAction}>
-        Click
-      </button>
-    );
-  }
-}
-
-Button = connect((state) => state)(Button);
 
 export default App;
